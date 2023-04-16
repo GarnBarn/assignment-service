0a772385b430303c09d1c445c41a154d7ab3c604
Siratee K <sirateek@gmail.com>
2023-04-16 23:57:45 +0700

    fix: Initlize the base application

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/GarnBarn/assignment-service/config"
+	"github.com/GarnBarn/common-go/httpserver"
+	"github.com/GarnBarn/common-go/logger"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	appConfig config.Config
+)
+
+func init() {
+	appConfig = config.Load()
+	logger.InitLogger(logger.Config{
+		Env: appConfig.Env,
+	})
+
+}
 
 func main() {
-	fmt.Println("Hello")
+	// Create the http server
+	httpServer := httpserver.NewHttpServer()
+
+	logrus.Info("Listening and serving HTTP on :", appConfig.HTTP_SERVER_PORT)
+	httpServer.Run(fmt.Sprint(":", appConfig.HTTP_SERVER_PORT))
 }
